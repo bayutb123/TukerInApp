762d666162d3d1244850d2030197ada8d659d0ad
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-10-25 00:23:21 +0700

    Upgrade compiler to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bayutb123.tukerin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bayutb123.tukerin"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
