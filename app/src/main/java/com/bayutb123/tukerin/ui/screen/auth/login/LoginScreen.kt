45030f292f47890fa0a2fc23a13cf3609b0b5b41
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-10-26 00:15:17 +0700

    Add alertdialog to login screen

--- app/src/main/java/com/bayutb123/tukerin/ui/screen/auth/login/LoginScreen.kt
+++ app/src/main/java/com/bayutb123/tukerin/ui/screen/auth/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.bayutb123.tukerin.ui.screen.auth.login
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,18 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,15 +29,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bayutb123.tukerin.ui.components.input.CustomTextField
 import com.bayutb123.tukerin.ui.components.input.FullWidthButton
+import com.bayutb123.tukerin.ui.components.view.AlertDialogWithNoCancel
 import com.bayutb123.tukerin.ui.components.view.Backgrounds
 import com.bayutb123.tukerin.ui.theme.TukerInTheme
 
@@ -45,6 +43,7 @@ import com.bayutb123.tukerin.ui.theme.TukerInTheme
 fun LoginScreen(
     modifier: Modifier = Modifier
 ) {
+    var isAlertVisible by rememberSaveable { mutableStateOf(false) }
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
     Scaffold { paddingValues ->
@@ -55,10 +54,21 @@ fun LoginScreen(
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
+            AnimatedVisibility(visible = isAlertVisible) {
+                AlertDialogWithNoCancel(
+                    title = "Invalid Credentials",
+                    message = "Please check your email and password",
+                    onDismiss = { isAlertVisible = !isAlertVisible },
+                    onConfirm = { isAlertVisible = !isAlertVisible })
+            }
             Column(
                 modifier = modifier.padding(horizontal = 16.dp),
             ) {
-                Text(text = "Welcome Back", style = MaterialTheme.typography.displaySmall, fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Welcome Back",
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Bold
+                )
                 Text(text = "Login to continue", style = MaterialTheme.typography.titleMedium)
                 Spacer(modifier = Modifier.height(16.dp))
                 CustomTextField(
@@ -87,7 +97,7 @@ fun LoginScreen(
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Column {
-                    FullWidthButton(onClick = { /*TODO*/ }, text = "Login")
+                    FullWidthButton(onClick = { isAlertVisible = !isAlertVisible }, text = "Login")
                     Row(
                         modifier = modifier
                             .fillMaxWidth()
@@ -96,7 +106,10 @@ fun LoginScreen(
                         horizontalArrangement = Arrangement.SpaceBetween,
                     ) {
                         TextButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Forgot Password?", style = MaterialTheme.typography.bodyMedium)
+                            Text(
+                                text = "Forgot Password?",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
                         }
                         TextButton(onClick = { /*TODO*/ }) {
                             Text(text = "Register", style = MaterialTheme.typography.bodyMedium)
