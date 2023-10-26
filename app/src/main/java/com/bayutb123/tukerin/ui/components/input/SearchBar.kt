c854e2bc9bab60b4607f10fc2c9357a25ba28679
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-10-27 00:39:31 +0700

    Add search bar component

---
+++ app/src/main/java/com/bayutb123/tukerin/ui/components/input/SearchBar.kt
@@ -0,0 +1,134 @@
+package com.bayutb123.tukerin.ui.components.input
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bayutb123.tukerin.ui.theme.TukerInTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomSearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit,
+    onQueryChange: (String) -> Unit,
+    query: String,
+    active: Boolean,
+    onActiveChange: (Boolean) -> Unit,
+    placeholder: String = "Search",
+    content: @Composable ColumnScope.() -> Unit,
+    mainContent: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .semantics { isTraversalGroup = true }) {
+        SearchBar(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.surface)
+                .align(Alignment.TopCenter)
+                .semantics { traversalIndex = -1f },
+            query = query,
+            onQueryChange = onQueryChange,
+            onSearch = onSearch,
+            active = active,
+            onActiveChange = onActiveChange,
+            placeholder = { Text(placeholder) },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+
+            ) {
+            content()
+        }
+        mainContent()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCustomSearchBar() {
+    TukerInTheme {
+        var text by rememberSaveable { mutableStateOf("") }
+        var active by rememberSaveable { mutableStateOf(false) }
+
+        CustomSearchBar(
+            query = text,
+            onQueryChange = { text = it },
+            active = active,
+            onActiveChange = { active = it },
+            onSearch = {
+                active = false
+            },
+            content = {
+                repeat(4) { idx ->
+                    val resultText = "Suggestion $idx"
+                    ListItem(
+                        headlineContent = { Text(resultText) },
+                        supportingContent = { Text("Additional info") },
+                        leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+                        modifier = Modifier
+                            .clickable {
+                                text = resultText
+                                active = false
+                            }
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 4.dp)
+                    )
+                }
+            },
+            mainContent = {
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        top = 72.dp,
+                        end = 16.dp,
+                        bottom = 16.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    val list = List(100) { "Text $it" }
+                    items(count = list.size) {
+                        Text(
+                            list[it],
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+            }
+        )
+
+
+    }
+}
