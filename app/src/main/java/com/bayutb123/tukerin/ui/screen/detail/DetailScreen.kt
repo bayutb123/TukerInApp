0a3f4556c7ca3bf6f4299c5d7b58acf1c2fe6290
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-11-19 02:43:12 +0700

    progress: DetailScreen

--- app/src/main/java/com/bayutb123/tukerin/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/bayutb123/tukerin/ui/screen/detail/DetailScreen.kt
@@ -3,16 +3,21 @@ package com.bayutb123.tukerin.ui.screen.detail
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,7 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,6 +35,7 @@ import coil.compose.AsyncImage
 import com.bayutb123.tukerin.BuildConfig
 import com.bayutb123.tukerin.ui.components.view.SellerCard
 import com.bayutb123.tukerin.ui.theme.TukerInTheme
+import com.bayutb123.tukerin.ui.utils.Currency
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,9 +43,10 @@ fun DetailScreen(
     modifier: Modifier = Modifier,
     onBackRequested: () -> Unit,
     onNavigationRequested: (route: String) -> Unit,
-    postId: Int = 12
+    postId: Int
 ) {
-    val context = LocalContext.current
+    // create scrollstate
+
     val viewModel = hiltViewModel<DetailViewModel>()
     val post = viewModel.post.collectAsStateWithLifecycle()
 
@@ -60,18 +67,40 @@ fun DetailScreen(
             )
         }
     ) { paddingValues ->
-        Box(modifier = modifier.padding(paddingValues = paddingValues)) {
-            post.value?.let { post ->
-                Column {
-                    Text(text = post.title)
-                    Text(text = post.address)
-                    post.images?.forEach { imageUrl ->
-                        AsyncImage(
-                            model = "${BuildConfig.apiUrl}/images/$imageUrl",
-                            contentDescription = post.title
-                        )
+        Box(
+            modifier = modifier
+                .padding(paddingValues = paddingValues)
+
+        ) {
+            Column(
+                modifier = modifier
+                    .verticalScroll(rememberScrollState())
+            ) {
+                post.value.let {
+                    it?.let {
+                        it.images?.let { it1 -> ImagesView(images = it1) }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(text = it.title)
+                            // price
+                            Text(
+                                text = Currency.convertIntToRupiah(it.price),
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            SellerCard(
+                                sellerName = it.ownerName,
+                                sellerLocation = it.address,
+                                sellerImage = "https://media.licdn.com/dms/image/D5603AQEQqJu4ohbltA/profile-displayphoto-shrink_200_200/0/1679033845873?e=1705536000&v=beta&t=w4rpjT0ZRRd1eo49L3TURJGqpXXmFVH7MXlJzLseg8I"
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(text = it.description)
+                        }
                     }
-                    SellerCard(sellerName = post.ownerName, sellerLocation = "", sellerImage = post.ownerName)
                 }
             }
         }
@@ -86,13 +115,16 @@ fun ImagesView(
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+            .height(250.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         itemsIndexed(images) { index, item ->
+            // build string contain image url
+            val imageUrl = BuildConfig.apiUrl + "/images/" + item
             AsyncImage(
-                model = item, contentDescription = index.toString(),
-                contentScale = ContentScale.Crop
+                model = imageUrl, contentDescription = index.toString(),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(250.dp)
             )
         }
     }
@@ -107,7 +139,8 @@ fun DetailScreenPreview() {
     TukerInTheme {
         DetailScreen(
             onBackRequested = {},
-            onNavigationRequested = {}
+            onNavigationRequested = {},
+            postId = 1
         )
     }
 }
