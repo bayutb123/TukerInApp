0acf7f51f7899101c06b834c8ef1c8e252ac1a23
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-11-18 14:18:49 +0700

    Implement detail page (not finished yet)

---
+++ app/src/main/java/com/bayutb123/tukerin/data/source/remote/response/auth/LoginResponse.kt
@@ -0,0 +1,42 @@
+package com.bayutb123.tukerin.data.source.remote.response.auth
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("user")
+	val user: LoginUser? = null
+)
+
+data class LoginUser(
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String,
+
+	@field:SerializedName("verified_at")
+	val verifiedAt: Any,
+
+	@field:SerializedName("api_token")
+	val apiToken: String,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("created_at")
+	val createdAt: String,
+
+	@field:SerializedName("is_premium_user")
+	val isPremiumUser: Int,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("profile_photo_path")
+	val profilePhotoPath: Any,
+
+	@field:SerializedName("email")
+	val email: String
+)
