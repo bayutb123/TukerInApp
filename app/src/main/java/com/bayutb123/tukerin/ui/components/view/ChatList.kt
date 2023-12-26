7bf9d8e5e857dc1a3f95526404ff4133a781d143
Bayu Tantra Bramandhita <73697375+bayutb123@users.noreply.github.com>
2023-12-26 12:20:31 +0700

    feature: add overline content to chat ListItem

--- app/src/main/java/com/bayutb123/tukerin/ui/components/view/ChatList.kt
+++ app/src/main/java/com/bayutb123/tukerin/ui/components/view/ChatList.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.bayutb123.tukerin.domain.model.Chat
 import com.bayutb123.tukerin.domain.model.Message
+import com.bayutb123.tukerin.domain.model.User
 import com.bayutb123.tukerin.ui.theme.TukerInTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,6 +41,9 @@ fun ChatList(
             items(items = data, key = { item -> item.id } ){ chat ->
                 ListItem(
                     headlineContent = { Text(text = chat.receiver.name) },
+                    overlineContent = {
+                        Text(text = chat.context)
+                    },
                     leadingContent = {
                         AsyncImage(
                             model = "",
@@ -66,6 +70,32 @@ fun ChatList(
 @Composable
 fun PreviewMessageList() {
     TukerInTheme {
-
+        ChatList(
+            data = listOf(
+                Chat(
+                    id = 1,
+                    receiver = User(
+                        id = 2,
+                        name = "Bayu",
+                        email = "",
+                        token = "",
+                        isPremium = false
+                    ),
+                    context = "Product",
+                    lastMessage = Message(
+                        id = 1,
+                        chatId = 1,
+                        senderId = 1,
+                        receiverId = 2,
+                        message = "Hello",
+                        attachment = null,
+                        isRead = false
+                    ),
+                    userId = 1,
+                    createdAt = "",
+                )
+            ),
+            onItemClick = {}
+        )
     }
 }
