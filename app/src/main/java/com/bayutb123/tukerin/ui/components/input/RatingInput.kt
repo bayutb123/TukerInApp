569eb1b621861ae838bcb78e865c5e5f3043a21f
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2024-05-08 23:31:15 +0700

    code: enhance RatingInput

--- app/src/main/java/com/bayutb123/tukerin/ui/components/input/RatingInput.kt
+++ app/src/main/java/com/bayutb123/tukerin/ui/components/input/RatingInput.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
@@ -16,6 +17,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -31,28 +33,24 @@ fun RatingInput(
     modifier: Modifier = Modifier,
     onChange: (Int) -> Unit
 ) {
-    var rating by rememberSaveable {
+    var rating by remember {
         mutableIntStateOf(0)
     }
-    Box(modifier = Modifier.padding(8.dp)) {
-        LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp), verticalAlignment = Alignment.CenterVertically) {
-            items(5) {
-                IconButton(onClick = {
-                    rating = it + 1
-                    onChange(rating)
-                }) {
-                    Icon(imageVector = Icons.Default.StarBorder, contentDescription = "StarBorder")
-                }
-            }
-        }
-        LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp), verticalAlignment = Alignment.CenterVertically) {
-            items(rating) {
-                IconButton(onClick = {
-                    rating = it + 1
-                    onChange(rating)
-                }) {
-                    Icon(imageVector = Icons.Default.Star, contentDescription = "Star", tint = Color.Yellow)
-                }
+    Row(
+        modifier = Modifier.padding(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (i in 1..5) {
+            IconButton(onClick = {
+                rating = i
+                onChange(rating)
+            }) {
+                Icon(
+                    imageVector = if (i <= rating) Icons.Default.Star else Icons.Default.StarBorder,
+                    contentDescription = "Rate $i stars",
+                    tint = if (i <= rating) Color.Yellow else Color.Unspecified
+                )
             }
         }
     }
