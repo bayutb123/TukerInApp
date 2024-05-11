467e5d4ee410bdd7dff1da658599c81e54a2295b
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2024-05-12 03:58:00 +0700

    prepare enum for post status

---
+++ core/src/main/java/com/bayutb123/tukerin/core/utils/PublishStatus.kt
@@ -0,0 +1,11 @@
+package com.bayutb123.tukerin.core.utils
+
+enum class PublishStatus {
+    NOT_PUBLISHED,
+    PUBLISHED,
+    TRANSACTION_PENDING,
+    TRANSACTION_COMPLETED,
+    TRANSACTION_CANCELLED,
+    NEEDS_REVIEW,
+    CLOSED
+}
